0b94596f6f5035d133f1bfeaa84caaf89bd25da4
AlexVelezLl <alanvele@espol.edu.ec>
2021-12-30 22:47:45 -0500

    Adding DDL

--- backend/db/DDL.js
+++ backend/db/DDL.js
@@ -1,16 +1,49 @@
-const {Spanner} = require('@google-cloud/spanner');
+const { Spanner } = require('@google-cloud/spanner');
 const { projectId, instanceId, databaseId } = require('./bd.config');
 
-const spanner = new Spanner({projectId});
+const spanner = new Spanner({ projectId });
 
 const instance = spanner.instance(instanceId);
 const database = instance.database(databaseId);
 
-const query = {
-  sql: 'SELECT 1',
-};
+const schema = [
+    `CREATE TABLE Flight (
+        flightID    STRING(36)  NOT NULL,
+        flightSource   STRING(20)  NOT NULL,
+        flightDest    STRING(20)  NOT NULL,
+        flightSeat  INT64  NOT NULL,
+        ticketCost FLOAT64  NOT NULL
+      ) PRIMARY KEY (flightID)`,
+      
+      `CREATE TABLE Booking (
+        flightID    STRING(36) NOT NULL,
+        bookingID    STRING(36) NOT NULL,
+        bookDate    DATE
+      ) PRIMARY KEY (flightID, bookingID),
+      INTERLEAVE IN PARENT Flight ON DELETE CASCADE`,
+  
+      `CREATE TABLE BookingDetails (
+        flightID    STRING(36) NOT NULL,
+        bookingID    STRING(36)  NOT NULL,
+        passengerID    STRING(36) NOT NULL
+        ) PRIMARY KEY (flightID, bookingID, passengerID),
+        INTERLEAVE IN PARENT Booking ON DELETE CASCADE`,
 
-database.run(query).then(rows=>{
-  console.log(`Query: ${rows.length} found.`);
-  rows.forEach(row => console.log(row));
-})
+      `CREATE TABLE Passenger (
+        passengerID    STRING(36) NOT NULL,
+        passengerName    STRING(20) NOT NULL,
+        passengerEmail    STRING(20) NOT NULL,
+        passengerDOB    DATE NOT NULL
+        ) PRIMARY KEY (passengerID)`
+];
+
+schema.forEach(async (sql) => {
+  try {
+    database.tabl
+    await database.updateSchema(sql);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+database.close();
