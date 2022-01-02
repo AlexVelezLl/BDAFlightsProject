41f4f0d6e6159308ed5d442d3330b41e10f93a95
AlexVelezLl <alanvele@espol.edu.ec>
2022-01-02 16:49:56 -0500

    using cors middleware

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -12,6 +13,7 @@ const bookingRouter = require('./routes/booking');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
