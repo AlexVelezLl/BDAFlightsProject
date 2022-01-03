ac0e9ef2e4e38222490cc39c8cd8e647177b1446
AlexVelezLl <alanvele@espol.edu.ec>
2022-01-03 17:40:17 -0500

    Data generation

---
+++ backend/db/generateData.js
@@ -0,0 +1,64 @@
+const random_name = require('node-random-name');
+const uuid = require('uuid');
+const fs = require('fs');
+
+function generatePassengers() {
+  const passengers = [];
+  for (let i = 0; i < 1000; i++) {
+    const id = uuid.v4();
+    const name = random_name({ random: Math.random });
+    const email = `${name.split(' ')[0]}@gmail.com`;
+    const dob = new Date(
+      Math.floor(Math.random() * (new Date().getFullYear() - 1900 + 1)) + 1900,
+      Math.floor(Math.random() * 12),
+      Math.floor(Math.random() * 28)
+    );
+    passengers.push({ id, name, email, dob });
+  }
+  fs.writeFileSync('db/data/passengers.json', JSON.stringify(passengers));
+}
+
+function generateFlights() {
+  const flights = [];
+  const cities = JSON.parse(fs.readFileSync('db/data/cities.json'));
+  for (let i = 0; i < 1000; i++) {
+    const id = uuid.v4();
+    const source = cities[Math.floor(Math.random() * cities.length)].name;
+    const dest = cities[Math.floor(Math.random() * cities.length)].name;
+    //Make variable arrivalDate between 1 and 30 days from now
+    const date = new Date(
+      new Date().getTime() +
+        Math.floor(Math.random() * (30 * 24 * 60 * 60 * 1000))
+    );
+    // Make variable seat and cost a random choice between 100, 200, 300, 400, 500, 600
+    const seat = Math.floor(Math.random() * 5) * 100 + 100;
+    const cost = Math.floor(Math.random() * 5) * 100 + 100;
+    flights.push({ id, source, dest, date, seat, cost });
+  }
+  fs.writeFileSync('db/data/flights.json', JSON.stringify(flights));
+}
+
+const generateBookings = () => {
+  const bookings = [];
+  const passengers = JSON.parse(fs.readFileSync('db/data/passengers.json'));
+  const flights = JSON.parse(fs.readFileSync('db/data/flights.json'));
+  for (let i = 0; i < 1000; i++) {
+    const id = uuid.v4();
+    const numberPassengers = Math.floor(Math.random() * 10) + 1;
+    const flight = flights[Math.floor(Math.random() * flights.length)].id;
+    const date = new Date(
+      new Date().getTime() -
+        Math.floor(Math.random() * (30 * 24 * 60 * 60 * 1000)) // Random between 1 and 30 days ago
+    );
+    const passengerIDs = new Set();
+    for (let j = 0; j < numberPassengers; j++) {
+      const passenger =
+        passengers[Math.floor(Math.random() * passengers.length)].id;
+      passengerIDs.add(passenger);
+    }
+    bookings.push({ id, passengerIDs: [...passengerIDs], date, flight });
+  }
+  fs.writeFileSync('db/data/bookings.json', JSON.stringify(bookings));
+};
+
+generateBookings();
---
+++ backend/db/insertData.js
@@ -0,0 +1,119 @@
+const fs = require('fs');
+const { Spanner } = require('@google-cloud/spanner');
+
+const { databaseId, instanceId, projectId } = require('./bd.config');
+
+const spanner = new Spanner({ projectId });
+
+const instance = spanner.instance(instanceId);
+const database = instance.database(databaseId);
+
+async function insertPassegners() {
+  const passengers = JSON.parse(fs.readFileSync('db/data/passengers.json'));
+
+  for (let passenger of passengers) {
+    const { id, name, email, dob } = passenger;
+    await new Promise((resolve, reject) => {
+      database.runTransaction(async (err, transaction) => {
+        if (err) reject(err);
+        try {
+          await transaction.runUpdate({
+            sql: `
+              INSERT
+                Passenger(passengerID, passengerName, passengerEmail, passengerDOB)
+              VALUES
+                (@id, @name, @email, @dob)`,
+            params: { id, name, email, dob },
+          });
+          await transaction.commit();
+          resolve();
+        } catch (err) {
+          transaction.rollback();
+          console.log('Error inserting passenger: ', id, '. Error: ', err);
+          reject(err);
+        }
+      });
+    });
+  }
+}
+
+async function insertFlights() {
+  const flights = JSON.parse(fs.readFileSync('db/data/flights.json'));
+
+  for (let flight of flights) {
+    const { id, source, dest, date, seat, cost } = flight;
+    await new Promise((resolve, reject) => {
+      database.runTransaction(async (err, transaction) => {
+        if (err) reject(err);
+        try {
+          await transaction.runUpdate({
+            sql: `
+              INSERT
+                Flight(flightID, flightSource, flightDest, flightDate, flightSeat, flightCost)
+              VALUES
+                (@id, @source, @dest, @date, @seat, @cost)`,
+            params: { id, source, dest, date, seat, cost },
+          });
+          await transaction.commit();
+          resolve();
+        } catch (err) {
+          transaction.rollback();
+          console.log('Error inserting flight: ', id, '. Error: ', err);
+          reject(err);
+        }
+      });
+    });
+  }
+}
+
+async function insertBookings() {
+  const bookings = JSON.parse(fs.readFileSync('db/data/bookings.json'));
+
+  for (let booking of bookings) {
+    const { id, date, flight, passengerIDs } = booking;
+    await new Promise((resolve, reject) => {
+      database.runTransaction(async (err, transaction) => {
+        if (err) reject(err);
+        try {
+          await transaction.runUpdate({
+            sql: `
+              INSERT
+                Booking(bookingID, flightID, bookingDate)
+              VALUES
+                (@id, @flight, @date)`,
+            params: { id, flight, date },
+          });
+          const passengerIDsObject = getPassengerIDsObject(passengerIDs);
+          await transaction.runUpdate({
+            sql:
+              `
+              INSERT
+                BookingDetails(flightID, bookingID, passengerID)
+              VALUES` +
+              passengerIDs
+                .map(
+                  (_, idx) => `
+                (@flight, @id, @passengerID${idx})`
+                )
+                .join(','),
+            params: { id, flight, ...passengerIDsObject },
+          });
+          await transaction.commit();
+          resolve();
+        } catch (err) {
+          console.log('Error inserting booking: ', id, '. Error: ', err);
+          reject(err);
+        }
+      });
+    });
+  }
+}
+
+const getPassengerIDsObject = (passengerIDs) => {
+  return passengerIDs.reduce((acc, passengerID, idx) => {
+    acc[`passengerID${idx}`] = passengerID;
+    return acc;
+  }, {});
+};
+
+database.close();
