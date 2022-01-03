c1b94cded44e36d3a30788f0d70168895c847e1c
Valeria Barzola <vbarzola@espol.edu.ec>
2022-01-02 22:12:55 -0500

    crud flight

--- frontend/pages/flight.vue
+++ frontend/pages/flight.vue
@@ -1,61 +1,82 @@
 <template lang="html">
   <v-app dark>
     <section class="flight">
-      <v-dialog v-model="addFlight" width="40%">
+      <v-dialog v-model="flightInfoModal" width="40%">
         <v-card outlined>
           <v-card-title class="white--text " style="background:#282c2c">
-            Agregar vuelo
+            {{ editFlight ? "Editar vuelo" : "Agregar vuelo" }}
           </v-card-title>
+          <v-progress-linear
+            :active="loadingFlightAction"
+            :indeterminate="loadingFlightAction"
+          ></v-progress-linear>
 
-          <v-form>
-            <v-row justify="center" class="my-4">
-              <v-col md="10" class="mx-2">
-                <v-text-field
-                  v-model="source"
-                  label="Origen"
-                  required
-                  outlined
-                ></v-text-field>
-                <v-text-field
-                  v-model="destination"
-                  label="Destino"
-                  required
-                  outlined
-                ></v-text-field>
-                <v-text-field
-                  v-model="date"
-                  label="Fecha"
-                  required
-                  type="date"
-                  outlined
-                ></v-text-field>
-                <div style="display:inline">
+          <v-form ref="form">
+            <v-card-text>
+              <v-row class="mt-0 mb-0 pb-0">
+                <v-col cols="6" class="pb-0">
                   <v-text-field
-                    v-model="seat"
+                    v-model="actualFlight.flightSource"
+                    label="Origen"
+                    required
+                    outlined
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="6" sm="6" md="6" lg="6" xl="6">
+                  <v-text-field
+                    v-model="actualFlight.flightDest"
+                    label="Destino"
+                    required
+                    outlined
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+
+              <v-row class="mt-0 mb-0 pb-0">
+                <v-col cols="6" sm="6" md="6" lg="6" xl="6">
+                  <v-text-field
+                    v-model="actualFlight.flightSeat"
                     label="Asiento"
                     required
                     outlined
                     type="number"
                   ></v-text-field>
+                </v-col>
+                <v-col cols="6" sm="6" md="6" lg="6" xl="6">
                   <v-text-field
-                    v-model="cost"
+                    v-model="actualFlight.ticketCost"
                     label="Costo del ticket"
                     required
                     outlined
                     type="number"
                   ></v-text-field>
-                </div>
-                <v-card-actions>
-                  <v-spacer />
-                  <v-btn color="grey darken-1" text>
-                    Cancelar
-                  </v-btn>
-                  <v-btn color="#648cac" text text>
-                    Guardar
-                  </v-btn>
-                </v-card-actions>
-              </v-col>
-            </v-row>
+                </v-col>
+              </v-row>
+              <v-text-field
+                v-model="actualFlight.flightDate"
+                label="Fecha"
+                required
+                type="date"
+                outlined
+              ></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="grey darken-1"
+                text
+                @click="flightInfoModal = false"
+              >
+                Cancelar
+              </v-btn>
+              <v-btn
+                color="#648cac"
+                text
+                @click="editFlight ? updateFlight() : addFlight()"
+              >
+                {{ editFlight ? "Editar" : "Agregar" }}
+              </v-btn>
+            </v-card-actions>
           </v-form>
         </v-card>
       </v-dialog>
@@ -68,6 +89,7 @@
               <v-icon> mdi-airplane-takeoff </v-icon>
               Vuelos</span
             >
+
             <v-btn
               fab
               absolute
@@ -76,11 +98,19 @@
               class="mt-2"
               color="#fad83a"
               dark
-              @click="addFlight = true"
+              @click="
+                flightInfoModal = true;
+                editFlight = false;
+                clearFlight();
+              "
             >
               <v-icon> mdi-airplane-plus </v-icon>
             </v-btn>
           </v-card-title>
+          <v-progress-linear
+            :active="loading"
+            :indeterminate="loading"
+          ></v-progress-linear>
 
           <v-simple-table outlined>
             <template v-slot:default>
@@ -96,11 +126,11 @@
               </thead>
               <tbody>
                 <tr v-for="item in flights" :key="item.name">
-                  <td class="text-center">{{ item.source }}</td>
-                  <td class="text-center">{{ item.destination }}</td>
-                  <td class="text-center">{{ item.date }}</td>
-                  <td class="text-center">{{ item.seats }}</td>
-                  <td class="text-center">{{ item.cost }}</td>
+                  <td class="text-center">{{ item.flightSource }}</td>
+                  <td class="text-center">{{ item.flightDest }}</td>
+                  <td class="text-center">{{ item.flightDate }}</td>
+                  <td class="text-center">{{ item.flightSeat }}</td>
+                  <td class="text-center">{{ item.ticketCost }}</td>
                   <td class="text-center">
                     <div>
                       <v-btn
@@ -108,7 +138,12 @@
                         dark
                         x-small
                         fab
-                        @click="dialog2 = true"
+                        @click="
+                          flightInfoModal = true;
+                          FlightToEdit = item.flightID;
+                          editFlight = true;
+                          setFlight(FlightToEdit);
+                        "
                       >
                         <v-icon> mdi-pencil </v-icon>
                       </v-btn>
@@ -138,50 +173,103 @@
     },
     data () {
       return {
+        loading: false,
+        loadingFlightAction: false,
+        flightInfoModal: false,
+        FlightToEdit: null,
+        editFlight: false,
+        flights: [],
+        actualFlight: {
+          flightSource: '',
+          flightDest: '',
+          flightDate: '',
+          flightSeat: '',
+          ticketCost: ''
+        },
 
-        addFlight: false,
-        flights: [
-          {
-            source: 'Mexico',
-            destination: 'Colombia',
-            date: '01/01/2020',
-            seats:'1',
-            cost:'$100'
-          },
-          {
-            source: 'Mexico',
-            destination: 'Colombia',
-            date: '01/01/2020',
-            seats:'1',
-            cost:'$100'
-          },
-          {
-            source: 'Mexico',
-            destination: 'Colombia',
-            date: '01/01/2020',
-            seats:'1',
-            cost:'$100'
-          },
-          {
-            source: 'Mexico',
-            destination: 'Colombia',
-            date: '01/01/2020',
-            seats:'1',
-            cost:'$100'
-          }
-        ],
-
-        dialog2: false,
-        source: '',
-        destination: '',
-        date: '',
-        seat: '',
-        cost: ''
-        
 
       }
     },
+    created () {
+      this.getFlights()
+    },
     methods: {
+      async getFlights(){
+        try {
+          this.loading = true;
+          const response = await this.$axios.get('flight');
+          this.flights = response.data.map(item => {
+            return {
+              flightID: item.flightID,
+              flightSource: item.flightSource,
+              flightDest: item.flightDest,
+              flightDate: item.flightDate.split('T')[0],
+              flightSeat: item.flightSeat,
+              ticketCost: item.ticketCost
+            }
+          });
+          this.loading = false;
+
+        } catch (error) {
+          this.loading = false;
+          console.log(error);
+        }
+      },
+
+      async addFlight(){
+        try {
+          this.loadingFlightAction = true;
+          const response = await this.$axios.post('flight', this.actualFlight);
+          this.getFlights();
+          this.loadingFlightAction = false;
+          this.flightInfoModal = false;
+          this.actualFlight = {
+            flightSource: '',
+            flightDest: '',
+            flightDate: '',
+            flightSeat: '',
+            ticketCost: ''
+          };
+          console.log(this.flights);
+
+        } catch (error) {
+          this.loading = false;
+          console.log(error);
+        }
+      },
+      setFlight(id){
+        console.log(this.flightToEdit);
+        this.actualFlight = {...this.flights.find(item => item.flightID === id)};
+        console.log(this.actualFlight);
+
+      },
+      async updateFlight(){
+        try {
+          this.loadingFlightAction = true;
+          console.log(this.actualFlight);
+          const response = await this.$axios.put('flight/' + this.FlightToEdit, this.actualFlight);
+          this.getFlights();
+          this.loadingFlightAction = false;
+          this.flightInfoModal = false;
+          console.log(this.flights);
+
+        } catch (error) {
+          this.loading = false;
+          console.log(error);
+        }
+      },
+
+      clearFlight(){
+        this.actualFlight = {
+          flightSource: '',
+          flightDest: '',
+          flightDate: '',
+          flightSeat: '',
+          ticketCost: ''
+        };
+      },
+
+
 
     },
     computed: {
@@ -189,8 +277,3 @@
     }
 }
 </script>
-
-<style scoped lang="scss">
-.flight {
-}
-</style>
